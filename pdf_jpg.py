b51253ab55f658d60401e300795c8549368e8da2
williezh <276971837@qq.com>
2017-05-10 19:07:31 +0800

    create exe file

--- pdf_jpg.py
+++ pdf_jpg.py
@@ -3,12 +3,19 @@ import os
 import sys  
 import PyPDF2  
 import PythonMagick 
+from time import sleep
+from shutil import copyfile
 from tkFileDialog import askopenfilename
 from os.path import exists, dirname, basename, splitext, isdir, join as joinpath
 from tqdm import tqdm
-  
-pdffilename = str(askopenfilename(filetypes=[(u'pdf格式', '*.pdf')]))  
-#pdffilename = "sample/xml.pdf"   
+from chardet import detect
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+absname = askopenfilename(filetypes=[(u'pdf格式', '*.pdf')])
+coding = 'gbk'  #detect(absname)
+pdffilename = joinpath(dirname(absname), 'images.pdf')  
+copyfile(absname, pdffilename)
 fname = basename(pdffilename)
 outputdir = joinpath(dirname(pdffilename), splitext(fname)[0])
 if not exists(outputdir) or not isdir(outputdir):
@@ -19,7 +26,11 @@ if not exists(outputdir) or not isdir(outputdir):
 pdf_im = PyPDF2.PdfFileReader(file(pdffilename, "rb"))
 npage = pdf_im.getNumPages()
 for p in tqdm(range(npage)):  
-    im = PythonMagick.Image(pdffilename + '[' + str(p) +']')  
+    im = PythonMagick.Image('{}[{}]'.format(pdffilename, p))  
     #im.density('300')  
     #im.read(pdffilename + '[' + str(p) +']')  
-    im.write(joinpath(outputdir, '{}.jpg'.format(p+1))) 
+    im.write(joinpath(outputdir, '{}.jpg'.format(p+1)).encode(coding)) 
+
+print(u'\n转换完毕。文件位于 {}\n\n'.format(outputdir))
+print(u'3秒后窗口自动关闭')
+sleep(3)
